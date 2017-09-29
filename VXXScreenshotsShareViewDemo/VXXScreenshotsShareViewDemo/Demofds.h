5a58bcf6c9700017fc2fcef9c0b539e3a96900ed
小星 <752061676@qq.com>
2017-09-29 15:18:27 +0800

    项目初始化

---
+++ VXXScreenshotsShareViewDemo/VXXScreenshotsShareViewDemo/Demofds.h
@@ -0,0 +1,13 @@
+//
+//  Demofds.h
+//  VXXScreenshotsShareViewDemo
+//
+//  Created by 小星 on 2017/9/29.
+//  Copyright © 2017年 小星. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Demofds : UIButton
+
+@end
---
+++ VXXScreenshotsShareViewDemo/VXXScreenshotsShareViewDemo/Demofds.m
@@ -0,0 +1,21 @@
+//
+//  Demofds.m
+//  VXXScreenshotsShareViewDemo
+//
+//  Created by 小星 on 2017/9/29.
+//  Copyright © 2017年 小星. All rights reserved.
+//
+
+#import "Demofds.h"
+
+@implementation Demofds
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
